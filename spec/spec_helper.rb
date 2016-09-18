1bb2028233abce71a7d4f39d84ab2dc2ff8a440f
andela-ydaniju <yusuf.daniju@andela.com>
2016-09-18 11:14:06 +0100

    add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 ::Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each do |f|
   require_relative f
